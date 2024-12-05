4f848a3e9c4da65ac9c0b2219066676b3debeb17
Michael Chavinda <mschavinda@gmail.com>
2024-12-04 21:09:04 -0800

    Support showing histograms for regular string types

--- src/Data/DataFrame/Display/Terminal.hs
+++ src/Data/DataFrame/Display/Terminal.hs
@@ -19,44 +19,47 @@ import Data.Typeable (Typeable)
 import Data.Type.Equality
     ( type (:~:)(Refl), TestEquality(testEquality) )
 import qualified Type.Reflection
+import GHC.Stack (HasCallStack)
 
-plotHistograms :: DI.DataFrame -> IO ()
+plotHistograms :: HasCallStack => DI.DataFrame -> IO ()
 plotHistograms df = do
     forM_ (Ops.columnNames df) $ \cname -> do
         plotForColumn cname ((M.!) (DI.columns df) cname) df
 
 
 -- Plot code adapted from: https://alexwlchan.net/2018/ascii-bar-charts/
-plotForColumn :: Str.ByteString -> DI.Column -> DI.DataFrame -> IO ()
+plotForColumn :: HasCallStack => Str.ByteString -> DI.Column -> DI.DataFrame -> IO ()
 plotForColumn cname (DI.MkColumn (column :: V.Vector a)) df = do
     let repa :: Type.Reflection.TypeRep a = Type.Reflection.typeRep @a
         repByteString :: Type.Reflection.TypeRep Str.ByteString = Type.Reflection.typeRep @Str.ByteString
         repString :: Type.Reflection.TypeRep String = Type.Reflection.typeRep @String
-    case repa `testEquality` repByteString of
-        Nothing -> pure ()
-        Just Refl -> do
-            putStrLn $ "\nHistogram for " ++ show cname ++ "\n"
-            let n = 8 :: Int
-            let counts = map (first show) $ Ops.valueCounts @a cname df
-            let maxValue = maximum $ map snd counts
-            let increment = maxValue `div` 50
-            let longestLabelLength = maximum $ map (length . fst) counts
-            let longestBar = fromIntegral $ (maxValue * fromIntegral n `div` increment) `div` fromIntegral n + 1
-            putStrLn $ "|" ++ replicate (longestLabelLength + length (show maxValue) + longestBar + 6) '-' ++ "|"
-            forM_ counts $ \(label, count) -> do
-                let barChunks = fromIntegral $ (count * fromIntegral n `div` increment) `div` fromIntegral n
-                let remainder = fromIntegral $ (count * fromIntegral n `div` increment) `rem` fromIntegral n
+    let counts = case repa `testEquality` repByteString of
+            Just Refl -> map (first show) $ Ops.valueCounts @Str.ByteString cname df
+            Nothing -> case repa `testEquality` repString of
+                Nothing -> []
+                Just Refl -> map (first show) $ Ops.valueCounts @String cname df
+    if null counts then pure () else plotGivenCounts cname counts
 
-                let fractional = ([chr (ord '█' + n - remainder) | remainder > 0])
-
-                let bar = replicate barChunks '█' ++ fractional
-
-                let disp = if null bar then "| " else bar
-                putStrLn $ "|" ++ brightGreen (rightJustify label longestLabelLength) ++ " | " ++
-                            rightJustify (show count) (length (show maxValue)) ++ " |" ++
-                            " " ++ brightBlue bar
-                putStrLn $ "|" ++ replicate (longestLabelLength + length (show maxValue) + longestBar + 6) '-' ++ "|"
-            putChar '\n'
+plotGivenCounts :: Str.ByteString -> [(String, Integer)] -> IO ()
+plotGivenCounts cname counts = do
+    putStrLn $ "\nHistogram for " ++ show cname ++ "\n"
+    let n = 8 :: Int
+    let maxValue = maximum $ map snd counts
+    let increment = max 1 (maxValue `div` 50)
+    let longestLabelLength = maximum $ map (length . fst) counts
+    let longestBar = fromIntegral $ (maxValue * fromIntegral n `div` increment) `div` fromIntegral n + 1
+    putStrLn $ "|" ++ replicate (longestLabelLength + length (show maxValue) + longestBar + 6) '-' ++ "|"
+    forM_ counts $ \(label, count) -> do
+        let barChunks = fromIntegral $ (count * fromIntegral n `div` increment) `div` fromIntegral n
+        let remainder = fromIntegral $ (count * fromIntegral n `div` increment) `rem` fromIntegral n
+        let fractional = ([chr (ord '█' + n - remainder) | remainder > 0])
+        let bar = replicate barChunks '█' ++ fractional
+        let disp = if null bar then "| " else bar
+        putStrLn $ "|" ++ brightGreen (rightJustify label longestLabelLength) ++ " | " ++
+                    rightJustify (show count) (length (show maxValue)) ++ " |" ++
+                    " " ++ brightBlue bar
+        putStrLn $ "|" ++ replicate (longestLabelLength + length (show maxValue) + longestBar + 6) '-' ++ "|"
+    putChar '\n'
 
 rightJustify :: String -> Int -> String
 rightJustify s n = replicate (max 0 (n - length s)) ' ' ++ s
