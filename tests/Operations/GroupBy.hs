000be19692879b896cd3c089c088e91bbc8bb666
Michael Chavinda <mschavinda@gmail.com>
2025-01-08 08:31:24 -0800

    Add intial groupby tests

--- tests/Operations/GroupBy.hs
+++ tests/Operations/GroupBy.hs
@@ -31,10 +31,37 @@ groupBySingleRowWAI = TestCase (assertEqual "Groups by single column"
                              ("test3", DI.GroupedUnboxedColumn (V.generate 4 (\i -> VU.fromList [(i * 10 + 1)..((i + 1) * 10)]))),
                              ("test4", DI.GroupedUnboxedColumn (V.generate 4 (\i -> VU.fromList [(((3 - i) + 1) * 10),(((3 - i) + 1) * 10 - 1)..((3 - i) * 10 + 1)])))
                             ])
-                (D.groupBy ["test1"] testData D.|> D.sortBy D.Ascending "test1"))
+                (D.groupBy ["test1"] testData D.|> D.sortBy D.Ascending ["test1"]))
+
+groupByMultipleRowsWAI :: Test
+groupByMultipleRowsWAI = TestCase (assertEqual "Groups by single column"
+                (D.fromList [("test1", DI.toColumn $ concatMap (replicate 2) [(1::Int)..4]),
+                             ("test2", DI.toColumn (take 8 $ cycle [1 :: Int, 2])),
+                             ("test3", DI.GroupedUnboxedColumn (V.fromList [
+                                        VU.fromList [1 :: Int,3..9],
+                                        VU.fromList [2,4..10],
+                                        VU.fromList [11,13..19],
+                                        VU.fromList [12,14..20],
+                                        VU.fromList [21,23..29],
+                                        VU.fromList [22,24..30],
+                                        VU.fromList [31,33..39],
+                                        VU.fromList [32,34..40]
+                                ])),
+                             ("test4", DI.GroupedUnboxedColumn (V.fromList $ reverse [
+                                        VU.fromList [1 :: Int,3..9],
+                                        VU.fromList [2,4..10],
+                                        VU.fromList [11,13..19],
+                                        VU.fromList [12,14..20],
+                                        VU.fromList [21,23..29],
+                                        VU.fromList [22,24..30],
+                                        VU.fromList [31,33..39],
+                                        VU.fromList [32,34..40]
+                                ]))
+                            ])
+                (D.groupBy ["test1", "test2"] testData D.|> D.sortBy D.Ascending ["test1", "test2"]))
 
 tests :: [Test]
 tests = [ TestLabel "groupBySingleRowWAI" groupBySingleRowWAI
-        , TestLabel "groupBySingleRowWAI" groupBySingleRowWAI
+        , TestLabel "groupByMultipleRowsWAI" groupByMultipleRowsWAI
         ]
 
