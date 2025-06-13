7c5868afa04815b6b44630b80d823bf324c09374
Michael Chavinda <mschavinda@gmail.com>
2025-06-13 00:42:48 -0700

    Add convenice comparison functions for 0.2.0.0

--- src/DataFrame/Internal/Expression.hs
+++ src/DataFrame/Internal/Expression.hs
@@ -29,8 +29,8 @@ import Data.Maybe (fromMaybe)
 data Expr a where
     Col :: Columnable a => T.Text -> Expr a 
     Lit :: Columnable a => a -> Expr a
-    Apply :: (Columnable a, ColumnifyRep (KindOf a) a, Columnable b, ColumnifyRep (KindOf b) b) => (b -> a) -> Expr b -> Expr a
-    BinOp :: (Columnable c, ColumnifyRep (KindOf c) c, Columnable b, ColumnifyRep (KindOf b) b, Columnable a, ColumnifyRep (KindOf a) a) => (c -> b -> a) -> Expr c -> Expr b -> Expr a
+    Apply :: (Columnable a, Columnable b) => (b -> a) -> Expr b -> Expr a
+    BinOp :: (Columnable c, Columnable b, Columnable a) => (c -> b -> a) -> Expr c -> Expr b -> Expr a
 
 interpret :: forall a b . (Columnable a) => DataFrame -> Expr a -> TypedColumn a
 interpret df (Lit value) = TColumn $ toColumn' $ V.replicate (fst $ dataframeDimensions df) value
@@ -45,7 +45,7 @@ interpret df (BinOp (f :: c -> d -> e) left right) = let
         (TColumn right') = interpret @d df right
     in TColumn $ zipWithColumns f left' right'
 
-instance (Num a, Columnable a, ColumnifyRep (KindOf a) a) => Num (Expr a) where
+instance (Num a, Columnable a) => Num (Expr a) where
     (+) :: Expr a -> Expr a -> Expr a
     (+) = BinOp (+)
 
@@ -64,14 +64,14 @@ instance (Num a, Columnable a, ColumnifyRep (KindOf a) a) => Num (Expr a) where
     signum :: Num a => Expr a -> Expr a
     signum = Apply signum
 
-instance (Fractional a, Columnable a, ColumnifyRep (KindOf a) a) => Fractional (Expr a) where
+instance (Fractional a, Columnable a) => Fractional (Expr a) where
     fromRational :: (Fractional a, Columnable a) => Rational -> Expr a
     fromRational = Lit . fromRational
 
     (/) :: (Fractional a, Columnable a) => Expr a -> Expr a -> Expr a
     (/) = BinOp (/)
 
-instance (Floating a, Columnable a, ColumnifyRep (KindOf a) a) => Floating (Expr a) where
+instance (Floating a, Columnable a) => Floating (Expr a) where
     pi :: (Floating a, Columnable a) => Expr a
     pi = Lit pi
     exp :: (Floating a, Columnable a) => Expr a -> Expr a
@@ -107,14 +107,29 @@ instance (Show a) => Show (Expr a) where
     show (Apply f value) = "apply(" ++ show value ++ ")"
     show (BinOp f a b) = "binop(" ++ show a ++ ", " ++ show b ++ ")" 
 
-col :: (Columnable a, ColumnifyRep (KindOf a) a) => T.Text -> Expr a
+col :: Columnable a => T.Text -> Expr a
 col = Col
 
-lit :: (Columnable a, ColumnifyRep (KindOf a) a) => a -> Expr a
+lit :: Columnable a => a -> Expr a
 lit = Lit
 
-lift :: (Columnable a, ColumnifyRep (KindOf a) a, Columnable b, ColumnifyRep (KindOf b) b) => (a -> b) -> Expr a -> Expr b
+lift :: (Columnable a, Columnable b) => (a -> b) -> Expr a -> Expr b
 lift = Apply
 
-lift2 :: (ColumnifyRep (KindOf c) c, Columnable c, ColumnifyRep (KindOf b) b, Columnable b, ColumnifyRep (KindOf a) a, Columnable a) => (c -> b -> a) -> Expr c -> Expr b -> Expr a 
+lift2 :: (Columnable c, Columnable b, Columnable a) => (c -> b -> a) -> Expr c -> Expr b -> Expr a 
 lift2 = BinOp
+
+eq :: (Columnable a, Eq a) => Expr a -> Expr a -> Expr Bool
+eq = BinOp (==)
+
+lt :: (Columnable a, Ord a) => Expr a -> Expr a -> Expr Bool
+lt = BinOp (<)
+
+gt :: (Columnable a, Ord a) => Expr a -> Expr a -> Expr Bool
+gt = BinOp (<)
+
+leq :: (Columnable a, Ord a, Eq a) => Expr a -> Expr a -> Expr Bool
+leq = BinOp (<=)
+
+geq :: (Columnable a, Ord a, Eq a) => Expr a -> Expr a -> Expr Bool
+geq = BinOp (<=)
