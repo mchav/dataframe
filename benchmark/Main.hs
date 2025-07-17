ef902cd1c3f0adfb5b149e4f6c949a4fcfbc6157
Michael Chavinda <mschavinda@gmail.com>
2025-07-17 16:19:01 -0700

    Add python benchmarks

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -3,31 +3,63 @@
 
 import qualified DataFrame as D
 import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector.Unboxed.Mutable as VUM
 
 import Control.Monad (replicateM)
 import Criterion.Main
 import Data.Time
+import System.Process
 import System.Random (randomRIO)
 
-stats :: Int -> IO ()
-stats n = do
-  startTime <- getCurrentTime
-  ns <- do
-    ns' <- VU.replicateM n (randomRIO (-20.0 :: Double, 20.0))
-    pure $ replicate 3 ns'
-  let df = D.fromUnamedColumns (map D.fromUnboxedVector ns)
-  
+haskell :: IO ()
+haskell = do
+  let n = 50_000_000
+  startGeneration <- getCurrentTime
+  ns <- runST $ do
+    ns' <- VUM.new 50_000_000
+    
+    VU.replicateM n (randomRIO (-20.0 :: Double, 20.0))
+  xs <- VU.replicateM n (randomRIO (-20.0 :: Double, 20.0))
+  ys <- VU.replicateM n (randomRIO (-20.0 :: Double, 20.0))
+  let df = D.fromUnamedColumns (map D.fromUnboxedVector [ns, xs, ys])
+  endGeneration <- getCurrentTime
+
+  let generationTime = diffUTCTime endGeneration startGeneration
+  putStrLn $ "Data generation Time: " ++ (show generationTime)
+
+  startCalculation <- getCurrentTime
   print $ D.mean "0" df
   print $ D.variance "1" df
   print $ D.correlation "1" "2" df
-  print $ D.filter "0" (>= (19.9 :: Double)) df D.|> D.take 10
-  endTime <- getCurrentTime
-  let diff = diffUTCTime endTime startTime
-  putStrLn $ "Execution Time: " ++ (show diff)
+  endCalculation <- getCurrentTime
 
-main = defaultMain [
-  bgroup "stats" [ bench     "300_000" $ nfIO (stats 100_000)
-                 , bench   "3_000_000" $ nfIO (stats 1_000_000)
-                 , bench "300_000_000" $ nfIO (stats 300_000_000)
-                 ]
-  ]
+  let calculationTime = diffUTCTime endCalculation startCalculation
+  putStrLn $ "Calculation Time: " ++ (show calculationTime)
+
+  startFilter <- getCurrentTime
+  print $ D.filter "0" (>= (19.9 :: Double)) df D.|> D.take 10
+  endFilter <- getCurrentTime
+
+  let filterTime = diffUTCTime endFilter startFilter
+  putStrLn $ "Filter Time: " ++ (show filterTime)
+
+  let totalTime = diffUTCTime endFilter startGeneration
+  putStrLn $ "Total Time: " ++ (show filterTime)
+
+polars :: IO ()
+polars = do
+  output <- readProcess "./benchmark/dataframe_benchmark/bin/python3" ["./benchmark/polars/polars_benchmark.py"] ""
+  putStrLn output
+
+pandas :: IO ()
+pandas = do
+  output <- readProcess "./benchmark/dataframe_benchmark/bin/python3" ["./benchmark/pandas/pandas_benchmark.py"] ""
+  putStrLn output
+
+main = do
+  defaultMain [
+    bgroup "stats" [ bench  "haskell" $ nfIO haskell
+                   , bench  "polars"  $ nfIO polars
+                   , bench  "pandas"  $ nfIO pandas
+                   ]
+    ]
---
+++ benchmark/polars/polars_benchmark.py
@@ -0,0 +1,41 @@
+import datetime
+import numpy as np
+import polars as pl
+
+# ------------------------------------------------------------------------------
+
+SIZE: int = 50_000_000
+
+first = datetime.datetime.now()
+df = pl.DataFrame({"normal": np.random.normal(size=SIZE),
+                   "log_normal": np.random.lognormal(size=SIZE),
+                   "exponential": np.random.exponential(size=SIZE),
+                   })
+second = datetime.datetime.now()
+print(f"Data generation/load time: "
+      f"{(second - first).seconds}.{(second - first).microseconds} secs")
+
+df2 = df.select(
+   mean = pl.col("normal").mean(),
+   var = pl.col("log_normal").var(),
+   corr = pl.corr("exponential", "log_normal")
+)
+
+mean: float = df2["mean"]
+var: float = df2["var"]
+corr: float = df2["corr"]
+
+print(f"{mean[0]}, {var[0]}, {corr[0]}")
+third = datetime.datetime.now()
+
+df3 = df.filter(pl.col("log_normal") > 8)
+print(df3.limit(10))
+print(f"Number of rows after select: {df3.select(pl.len()).item()}")
+fourth = datetime.datetime.now()
+
+fifth = datetime.datetime.now()
+
+print(f"Calculation time: {(third - second).seconds}.{(third - second).microseconds} secs")
+print(f"Selection time: {(fourth - third).seconds}.{(fourth - third).microseconds} secs")
+print(f"Overall time: {(fifth - first).seconds}.{(fifth - first).microseconds} secs")
+
