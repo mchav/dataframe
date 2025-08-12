eb5150c238a942de5ad42efe4fcc6ac3f7b94df0
Michael Chavinda <mschavinda@gmail.com>
2025-08-12 12:50:34 -0700

    refactor: Clean up hasktorch example

--- examples/CaliforniaHousing.hs
+++ examples/CaliforniaHousing.hs
@@ -19,24 +19,22 @@ import DataFrame ((|>))
 main :: IO ()
 main = do
     {- Feature ingestion and engineering -}
-    df <- fmap (D.apply (\(op :: T.Text) -> oceanProximity M.! op) "ocean_proximity") (D.readCsv "./data/housing.csv")
-    -- This column has nulls so we:
-    --  * Remove all nulls with filterJust
-    --  * Calculate the mean of total_bedrooms
-    --  * impute the mean.
-    -- This could probably be a utility function.
-    let meanTotalBedrooms = fromMaybe 0 $ df |> D.filterJust "total_bedrooms"
-                                             |> D.mean "total_bedrooms"
-        imputed = df |> D.impute "total_bedrooms" meanTotalBedrooms
+    df <- D.readCsv "./data/housing.csv"
+
+    let cleaned = df |> D.impute "total_bedrooms" (fromMaybe 0 (D.mean "total_bedrooms" df))
                      |> D.exclude ["median_house_value"]
+                     |> D.derive "ocean_proximity" (F.lift oceanProximity (F.col "ocean_proximity"))
+                     |> D.derive "rooms_per_household" (F.col @Double "total_rooms" / F.col "households")
                      |> normalizeFeatures
-        (r, c) = D.dimensions imputed
-        features = reshape [r,c] $ asTensor (flattenFeatures imputed)
+
+    -- Convert to hasktorch tensor
+        (r, c) = D.dimensions cleaned
+        features = reshape [r,c] $ asTensor (flattenFeatures cleaned)
         labels = asTensor ((VU.map realToFrac . VU.convert) (D.columnAsVector @Double "median_house_value" df) :: VU.Vector Float)
     
     {- Train the model -}
     putStrLn "Training linear regression model..."
-    init <- sample $ LinearSpec{in_features = (snd (D.dimensions df) - 1), out_features = 1}
+    init <- sample $ LinearSpec{in_features = c, out_features = 1}
     trained <- foldLoop init 100_000 $ \state i -> do
         let labels' = model state features
             loss = mseLoss labels labels'
@@ -51,8 +49,7 @@ main = do
 
 normalizeFeatures :: D.DataFrame -> D.DataFrame
 normalizeFeatures df = df |> D.fold (\name d -> let
-    m      = fromMaybe 0 (D.mean name d)
-    stdDev = fromMaybe 0.01 (D.standardDeviation name d)
+    -- Convenience reference to the column.
     col    = F.col @Double name
   in D.derive name ((col - (F.minimum col)) / (F.maximum col - F.minimum col)) d) (D.columnNames df)
 
@@ -60,8 +57,14 @@ normalizeFeatures df = df |> D.fold (\name d -> let
 model :: Linear -> Tensor -> Tensor
 model state input = squeezeAll $ linear state input
 
-oceanProximity :: M.Map T.Text Double
-oceanProximity = M.fromList [("ISLAND", 0), ("NEAR OCEAN", 1), ("NEAR BAY", 2), ("<1H OCEAN", 3), ("INLAND", 4)] 
+oceanProximity :: T.Text -> Double
+oceanProximity op = case op of
+    "ISLAND"     -> 0
+    "NEAR OCEAN" -> 1
+    "NEAR BAY"   -> 2
+    "<1H OCEAN"  -> 3
+    "INLAND"     -> 4
+    _            -> error ("Unknown ocean proximity value: " ++ (T.unpack op))
 
 flattenFeatures :: D.DataFrame -> VU.Vector Float
 flattenFeatures df = V.foldl' (\acc v -> acc VU.++ v) VU.empty (D.toMatrix df)
