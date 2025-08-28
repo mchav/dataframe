d05dbe1534aa217f26756790be19027d11553103
Michael Chavinda <michael.chavinda@fisglobal.com>
2025-08-28 09:59:17 -0700

    chore: Run update before repl in dataframe script.

--- scripts/dataframe.sh
+++ scripts/dataframe.sh
@@ -4,5 +4,7 @@ if [ ! -f ./dataframe.ghci ]; then
     curl --output dataframe.ghci "https://raw.githubusercontent.com/mchav/dataframe/refs/heads/main/dataframe.ghci"
 fi
 
+cabal update
+
 cabal repl -O2 --build-depends dataframe --repl-options="-ghci-script ./dataframe.ghci"
 
