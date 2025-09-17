c0204ddedd3c88ac92891aeaa02c2d82ecd9da83
Pierre-Alain Castella <pierrealain.castella@gmail.com>
2025-09-17 23:23:15 +0200

    feat: Add script to set up and run benchmark.

--- benchmark/explorer/mix.exs
+++ benchmark/explorer/mix.exs
@@ -5,15 +5,18 @@ defmodule Benchmark.MixProject do
     [
       app: :benchmark,
       version: "0.1.0",
-      deps: deps()
+      deps: deps(),
+      lockfile: Path.expand("mix.lock", __DIR__),
+      deps_path: Path.expand("deps", __DIR__),
+      build_path: Path.expand("_build", __DIR__)
     ]
   end
 
   defp deps do
     [
-      {:exla, "~> 0.10.0"},
+      #{:exla, "~> 0.10.0"},
       {:explorer, "~> 0.11.1"},
-      {:nx, "~> 0.10.0"}
+      #{:nx, "~> 0.10.0"}
     ]
   end
 end
