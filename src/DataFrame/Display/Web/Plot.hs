5ca1d5e03702bc88d8b9cfe5684507d122400b87
Michael Chavinda <mschavinda@gmail.com>
2025-09-26 20:18:30 -0700

    chore: Fix formatting

--- src/DataFrame/Display/Web/Plot.hs
+++ src/DataFrame/Display/Web/Plot.hs
@@ -72,7 +72,8 @@ defaultPlotConfig ptype =
         }
 
 chartJsScript :: T.Text
-chartJsScript = "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js\"></script>\n"
+chartJsScript =
+    "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js\"></script>\n"
 
 generateChartId :: IO T.Text
 generateChartId = do
