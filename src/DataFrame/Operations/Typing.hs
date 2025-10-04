6138f38fa690ebc2bf08c9189a144d40d61bbab1
Michael Chavinda <mschavinda@gmail.com>
2025-10-04 09:28:40 -0700

    fix: Convert default parse to text if given string.

--- src/DataFrame/Operations/Typing.hs
+++ src/DataFrame/Operations/Typing.hs
@@ -49,7 +49,7 @@ parseDefault safeRead dateFormat (BoxedColumn (c :: V.Vector a)) =
                         hasNulls =
                             V.foldl' (\acc v -> if isNothing v then acc || True else acc) False safeVector
                      in
-                        if safeRead && hasNulls then BoxedColumn safeVector else BoxedColumn c
+                        if safeRead && hasNulls then BoxedColumn safeVector else BoxedColumn (V.map T.pack c)
                 Nothing -> BoxedColumn c
             Just Refl ->
                 let example = T.strip (V.head c)
