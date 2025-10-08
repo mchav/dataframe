dd67b0d32be3393cb081fa9f9a5a84d4931f156d
Michael Chavinda <mschavinda@gmail.com>
2025-10-07 21:18:21 -0500

    feat: Add dataframe-hasktorch library (#97)
    Provides converts to and from hasktorch.

---
+++ dataframe-hasktorch/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for dataframe-hasktorch
+
+## 0.1.0.0
+
+* Export `toTensor` function that converts a dataframe to a tensor.
---
+++ dataframe-hasktorch/src/DataFrame/Hasktorch.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE BangPatterns #-}
+
+module DataFrame.Hasktorch (
+    toTensor,
+) where
+
+import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector.Unboxed.Mutable as VUM
+import qualified DataFrame as D
+
+import Control.Exception (throw)
+import DataFrame (DataFrame)
+import Torch
+
+toTensor :: DataFrame -> Tensor
+toTensor df = case D.toMatrix df of
+    Left e -> throw e
+    Right m ->
+        let
+            (r, c) = D.dimensions df
+            dims' = if c == 1 then [r] else [r, c]
+         in
+            reshape dims' (asTensor (flattenFeatures m))
+
+flattenFeatures :: V.Vector (VU.Vector Float) -> VU.Vector Float
+flattenFeatures rows =
+    let
+        total = V.foldl' (\s v -> s + VU.length v) 0 rows
+     in
+        VU.create $ do
+            ret <- VUM.unsafeNew total
+            let go !i !off
+                    | i == V.length rows = pure ()
+                    | otherwise = do
+                        let v = rows V.! i
+                            len = VU.length v
+                        VU.unsafeCopy (VUM.unsafeSlice off len ret) v
+                        go (i + 1) (off + len)
+            go 0 0
+            pure ret
