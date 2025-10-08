56162832a2a5cf72bfb554a25d263b9a32fad2d9
Michael Chavinda <mschavinda@gmail.com>
2025-10-07 19:34:54 -0700

    chore: Update california_housing to use `dataframe-hasktorch` .

--- examples/CaliforniaHousing.hs
+++ examples/CaliforniaHousing.hs
@@ -14,10 +14,9 @@ import Data.Maybe
 import qualified Data.Text as T
 import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as VU
-import qualified Data.Vector.Unboxed.Mutable as VUM
 import qualified DataFrame as D
 import qualified DataFrame.Functions as F
-import qualified DataFrame.Internal.Expression as F
+import DataFrame.Hasktorch (toTensor)
 import Torch
 
 import DataFrame ((|>))
@@ -38,8 +37,8 @@ main = do
                 |> normalizeFeatures
 
         -- Convert to hasktorch tensor
-        features = either throw id (toTensor cleaned)
-        labels = either throw id (toTensor (df |> D.select ["median_house_value"]))
+        features = toTensor cleaned
+        labels = toTensor (D.select ["median_house_value"] df)
 
     {- Train the model -}
     putStrLn "Training linear regression model..."
@@ -87,30 +86,3 @@ oceanProximity op = case op of
     "<1H OCEAN" -> 3
     "INLAND" -> 4
     _ -> error ("Unknown ocean proximity value: " ++ T.unpack op)
-
-toTensor :: D.DataFrame -> Either D.DataFrameException Tensor
-toTensor df = case D.toMatrix df of
-    Left e -> Left e
-    Right m ->
-        let
-            (r, c) = D.dimensions df
-            dims' = if c == 1 then [r] else [r, c]
-         in
-            Right (reshape dims' (asTensor (flattenFeatures m)))
-
-flattenFeatures :: V.Vector (VU.Vector Float) -> VU.Vector Float
-flattenFeatures rows =
-    let
-        total = V.foldl' (\s v -> s + VU.length v) 0 rows
-     in
-        VU.create $ do
-            mv <- VUM.unsafeNew total
-            let go !i !off
-                    | i == V.length rows = pure ()
-                    | otherwise = do
-                        let v = rows V.! i
-                            len = VU.length v
-                        VU.unsafeCopy (VUM.unsafeSlice off len mv) v
-                        go (i + 1) (off + len)
-            go 0 0
-            pure mv
