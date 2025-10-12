c12b363b0a7c4e85fc19a965cbbb5902fc5c661d
Michael Chavinda <mschavinda@gmail.com>
2025-10-12 01:21:56 -0700

    fix: Formatting as usual

--- src/DataFrame/Display.hs
+++ src/DataFrame/Display.hs
@@ -1,21 +1,22 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
 {-# HLINT ignore "Use newtype instead of data" #-}
 module DataFrame.Display where
 
-import qualified DataFrame.Operations.Subset as D
-import qualified DataFrame.Internal.DataFrame as D
 import qualified Data.Text.IO as T
+import qualified DataFrame.Internal.DataFrame as D
+import qualified DataFrame.Operations.Subset as D
 
 import Data.Function
 
-data DisplayOptions = DisplayOptions {
-    {-- | Maximum number of rows to render.
-    --
-    --   * If this value is less than or equal to 0, no rows are printed.
-    --   * If it is greater than the number of rows in the frame, all rows are printed.
-    --}
-    displayRows :: Int
-}
+data DisplayOptions = DisplayOptions
+    { {-- | Maximum number of rows to render.
+      --
+      --   * If this value is less than or equal to 0, no rows are printed.
+      --   * If it is greater than the number of rows in the frame, all rows are printed.
+      --}
+      displayRows :: Int
+    }
 
 defaultDisplayOptions :: DisplayOptions
 defaultDisplayOptions = DisplayOptions 10
