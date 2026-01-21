5c64817f797c572394e7a32be79f91893d819a65
Michael Coady <mcoady@gmail.com>
2026-01-21 02:10:10 +0000

    fix `writeCsv` for `OptionalColumn`  writing `Maybe a` values instead of `a` values

--- src/DataFrame/Lazy/IO/CSV.hs
+++ src/DataFrame/Lazy/IO/CSV.hs
@@ -383,11 +383,7 @@ getRowAsText df i = V.ifoldr go [] (columns df)
                     ++ "the other columns at index "
                     ++ show i
     go k (OptionalColumn (c :: V.Vector (Maybe a))) acc = case c V.!? i of
-        Just e -> textRep : acc
-          where
-            textRep = case testEquality (typeRep @a) (typeRep @T.Text) of
-                Just Refl -> fromMaybe "Nothing" e
-                Nothing -> (T.pack . show) e
+        Just e -> maybe T.empty T.show e : acc
         Nothing ->
             error $
                 "Column "
